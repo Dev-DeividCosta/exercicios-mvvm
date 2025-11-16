c85333d107a0b61e7d640a5211da19e91b8496ef
Dev-DeividCosta <dev.deivid.costa@gmail.com>
2025-11-16 10:41:52 -0300

    refactor(domain): simplify Produto6 model and remove redundant comments

--- app/src/main/java/com/example/myapplication/domain/model/Produto6.kt
+++ app/src/main/java/com/example/myapplication/domain/model/Produto6.kt
@@ -1,24 +1,11 @@
 package com.example.myapplication.domain.model
 
-// As respostas do checklist viraram este Model:
 data class Produto6(
-    // Identidade -> Produto
-
-    // Propriedades e Tipos de Dados
     val id: String,
     val nome: String,
     val preco: Double,
     val descricao: String,
     val urlImagem: String
 ) {
-    // Construtor vazio (baseado nas Restrições/Padrões)
-    // O Firestore precisa disso!
-    constructor() : this(
-        id ="",
-        nome = "",
-        preco = 0.0,
-        descricao = "",
-        urlImagem = ""
-    )
+    constructor() : this("", "", 0.0, "", "")
 }
-
