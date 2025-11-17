a773df42b754964b30429693b6597179218341cd
Dev-DeividCosta <dev.deivid.costa@gmail.com>
2025-11-17 13:35:19 -0300

    feat(domain) add SaleRepository

---
+++ app/src/main/java/com/example/myapplication/domain/repository/SaleRepository.kt
@@ -0,0 +1,14 @@
+package com.example.padrecicero.domain.repository
+
+import com.example.myapplication.domain.model.Sale
+import kotlinx.coroutines.flow.Flow
+
+interface SaleRepository {
+    suspend fun createSale(sale: Sale)
+    fun getAllSales(): Flow<List<Sale>>
+    fun getSaleById(saleId: String): Flow<Sale?>
+    fun getSalesByClientId(clientId: String): Flow<List<Sale>>
+    fun getSalesBySellerId(sellerId: String): Flow<List<Sale>>
+    suspend fun updateSale(sale: Sale)
+    suspend fun deleteSale(saleId: String)
+}
