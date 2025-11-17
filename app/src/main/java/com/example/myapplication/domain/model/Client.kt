f1e6ef93f4612d3eaee0ce5d9661679164d6f103
Dev-DeividCosta <dev.deivid.costa@gmail.com>
2025-11-17 13:24:58 -0300

    feat(domain): add Cliente model

---
+++ app/src/main/java/com/example/myapplication/domain/model/Client.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.domain.model
+
+data class Client(
+    val id: String,
+    val name: String,
+    val cpf: String,
+    val phone: String,
+    val address: Address
+) {
+    constructor() : this(
+        "", "", "", "", Address()
+    )
+}
