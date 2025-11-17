6ffc49f7b52a3be186e7d793f7c9f93a6a2f9fc7
Dev-DeividCosta <dev.deivid.costa@gmail.com>
2025-11-17 13:37:15 -0300

    fat(domain): add UserRepository

---
+++ app/src/main/java/com/example/myapplication/domain/repository/UserRepository.kt
@@ -0,0 +1,13 @@
+package com.example.padrecicero.domain.repository
+
+import com.example.myapplication.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+    suspend fun createUser(user: User)
+    fun getAllUsers(): Flow<List<User>>
+    fun getUserById(userId: String): Flow<User?>
+    fun getUsersByRole(role: String): Flow<List<User>> // Ex: "VENDEDOR"
+    suspend fun updateUser(user: User)
+    suspend fun deleteUser(userId: String)
+}
